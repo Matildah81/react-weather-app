8b4af6fd48c238429fb71ebed022abf1e5777e81
Matildah81 <matildahnkambule@gmail.com>
2023-08-18 12:45:31 +0200

    Added GitHub Repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 
 export default function App() {
   return(
-    <div className="App">Weather App
+    <div className="App">
+
+      <h1>Weather App</h1>
+
+    <footer>
+      This project was coded by <a href='https://www.matildahnkambule.com' target='_blank'>Matildah Nkambule</a> and is {""}
+        <a href='https://github.com/Matildah81/react-weather-app' target='_blank'>Open-Sourced on Gitub</a>
+      
+    </footer>
      
   </div>);
   
