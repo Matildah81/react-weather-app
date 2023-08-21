47bdeb31c8c33311a2c55c9ba80978af323b1130
Matildah81 <matildahnkambule@gmail.com>
2023-08-21 22:51:07 +0200

    opened a new file Weather.css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import Weather from './Weather';
 import './App.css';
 
 export default function App() {
   return(
     <div className="App">
+      <div className='container'>
 
-      <h1>Weather App</h1>
+      
+      <Weather/>
 
     <footer>
       This project was coded by <a href='https://www.matildahnkambule.com' target='_blank'>Matildah Nkambule</a> and is {""}
         <a href='https://github.com/Matildah81/react-weather-app' target='_blank'>Open-Sourced on Gitub</a>
       
     </footer>
-     
+     </div>
   </div>);
   
 }
